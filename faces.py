d5377ce4261e6f9c040d966ee9917a0831f298f1
EUNJI KIM <kej12516@gmail.com>
2019-05-27 21:40:33 +0900

    Update faces.py

--- faces.py
+++ faces.py
@@ -5,20 +5,12 @@ import cv2
 
 from data import FaceData
 
-# 이미지의 얼굴 탐지기 구현 
 class FaceDetector:
-
-# 이미지에서 얼굴을 감지하는데 사용되는 dilb 오브젝트의 인스턴스 
     _detector = None
-
-# 얼굴의 위치를 예측하는데 사용되는 dilb 오브젝트의 인스턴스 
     _predictor = None
   
 # 주어진 이미지에서 얼굴을 자동으로 탐지 
-# dilb 패키지의 얼굴 검출기 / 예측자 를 사용 
     def detect(self, image, downSampleRatio = None):
-   
-# 얼굴을 검색할 이미지 데이터 
         if FaceDetector._detector is None or FaceDetector._predictor is None:
             FaceDetector._detector = dlib.get_frontal_face_detector()
 
@@ -26,8 +18,6 @@ class FaceDetector:
                             .format(os.path.dirname(__file__)))
             FaceDetector._predictor = dlib.shape_predictor(faceModel)
 
-# 요청이 있는 경우 개선을 위해 원본 이미지의 크기를 줄임 
-# 초기 얼굴 검출 서능
         if downSampleRatio is not None:
             detImage = cv2.resize(image, (0, 0), fx=1.0 / downSampleRatio,
                                                  fy=1.0 / downSampleRatio)
@@ -42,8 +32,6 @@ class FaceDetector:
 # 많은 얼굴이 발견 되더라도 첫 번째 얼굴만 고려
         region = detectedFaces[0]
 
-# 다운 스케일링이 요청된 경우 감지된 영역을 축소
-# 해상도가 약간 낮아도 랜드마크는 이미지를 찾을 수 있음
         if downSampleRatio is not None:
             region = dlib.rectangle(region.left() * downSampleRatio,
                                     region.top() * downSampleRatio,
@@ -52,12 +40,8 @@ class FaceDetector:
 
 # 얼굴 영역에 모양 모델을 맞추어 위치를 예측 
         faceShape = FaceDetector._predictor(image, region)
-
-# 데이터 반환 
         face = FaceData()
 
-# 예측 된 랜드마크 위치로 오브젝트 데이터를 업데이트 
-# 경계 상자 그리기 
         face.landmarks = np.array([[p.x, p.y] for p in faceShape.parts()])
 
         margin = 10
